bd0421f11f094e94ca4d853f812aa4902e0d23cd
Itssuraj-1 <thapasuraj3215@gmail.com>
2025-05-08 09:48:07 +0545

    javaexception and public class

--- Boy.java
+++ Boy.java
@@ -1,17 +1,16 @@
-public class Human{
-    //overridden method
-    public void eat(){
+class Human {
+    public void eat() {
         System.out.println("Human is Eating");
     }
 }
-public class Boy extends Human{
-    //overriding method
-    public void eat(){
+
+public class Boy extends Human {
+    public void eat() {
         System.out.println("Boy is eating");
     }
+
     public static void main(String[] args) {
         Boy obj = new Boy();
-        //this will call the child class method
         obj.eat();
     }
-    }
+}
--- javaexception.java
+++ javaexception.java
@@ -1,13 +1,12 @@
-public class javaexception {
-    public static void main(String args[]){
-        try{
-            //code that may raise exception
-            int data = 100/0;
-        }
-        catch(ArithmeticException e){
+public class JavaException {
+    public static void main(String args[]) {
+        try {
+            // code that may raise exception
+            int data = 100 / 0;
+        } catch (ArithmeticException e) {
             System.out.println(e);
         }
-        //rest code of the program
+        // rest code of the program
         System.out.println("rest of the code...");
     }
 }
